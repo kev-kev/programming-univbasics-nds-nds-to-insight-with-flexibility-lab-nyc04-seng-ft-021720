9faf5263c83e92f67c6185a3a8499570489ac5a5
Kevin Clark <kevin.clark93@gmail.com>
2020-02-07 20:16:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -123,9 +123,8 @@ def movies_with_directors_set(source)
     while movie_index < source[director_index][:movies].length do
       inner_hash = {}
       inner_array = []
-      title = source[director_index][:movies][movie_index][:title]
-      movie_gross
-      inner_hash[:title] = title
+      inner_hash[:title] = source[director_index][:movies][movie_index][:title]
+      inner_hash[:worldwide_gross] = source[director_index[:movies][movie_index][:worldwide_gross]
       inner_hash[:director_name] = name
       inner_array.push(inner_hash)
       result.push(inner_array)
